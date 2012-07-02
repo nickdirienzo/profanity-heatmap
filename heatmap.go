1263f1f4ffb17f91021ccb91298dbbd2dd8f5a9b
Nick DiRienzo <nick@nickdirienzo.com>
2012-07-02 00:02:15 -0700

    Have commit locations. Started some modularization.

--- heatmap.go
+++ heatmap.go
@@ -6,40 +6,70 @@ import (
 	"fmt"
 	"io"
 	"log"
-	//"net/http"
+	"net/http"
 	"os"
 )
 
 type Event struct {
 	Payload Payload `json:"payload"`
-	Type  string  `json:"type"`
+    Actor string `json:"actor"`
+	Type    string  `json:"type"`
 }
 
 type Payload struct {
-    Shas []interface{} `json:"shas"`
+	Shas []interface{} `json:"shas"`
+}
+
+type User struct {
+    Location string `json:"location"`
+}
+
+const GITHUB string = "https://api.github.com"
+
+func (e *Event) GetActorLocation() (string, error) {
+    param := fmt.Sprint("/users/%q", e.Actor)
+    resp, err := http.Get(GITHUB + param)
+    if err != nil {
+        return "", err
+    }
+    decoder := json.NewDecoder(resp.Body)
+    var user User
+    for {
+        if err := decoder.Decode(&user); err == io.EOF {
+            break
+        } else if err != nil {
+            return "", err
+        }
+    }
+    return user.Location, nil
+}
+
+func GetTimeline(year int, month int, day int, hour int) (*Reader, error) {
+    url := fmt.Sprint("http://data.githubarchive.org/%d-%d-%d-%d.json.gz", year, month, day, hour)
+    resp, err := http.Get(url)
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()
+    reader, err := gzip.NewReader(resp.Body)
+    if err != nil {
+        return nil, err
+    }
+    return reader, nil
 }
 
 func main() {
-	/*resp, err := http.Get("http://data.githubarchive.org/2012-03-11-12.json.gz")
-	if err != nil {
-		fmt.Println("Couldn't retrieve data from GitHub Archive")
-		return
-	}
-	defer resp.Body.Close()
-	reader, err := gzip.NewReader(resp.Body)*/
 	file, err := os.Open("2012-03-11-15.json.gz")
 	if err != nil {
-		fmt.Println("Couldn't open file")
-		return
+		return err
 	}
 	reader, err := gzip.NewReader(file)
 	if err != nil {
-		fmt.Println("Couldn't uncompress")
-		return
+		return err
 	}
 	decoder := json.NewDecoder(reader)
-    var event Event
-	for i := 0; i < 10; i++ {
+	var event Event
+	for i := 0; i < 5; i++ {
 		if err := decoder.Decode(&event); err == io.EOF {
 			break
 		} else if err != nil {
@@ -47,15 +77,20 @@ func main() {
 		}
 
 		if event.Type == "PushEvent" {
-            for _, commit := range event.Payload.Shas {
-                switch commitData := commit.(type) {
-                    case []interface{}:
-                        fmt.Println("Commit Message:", commitData[2])
-                    default:
-                        log.Fatal("Could not convert Shas to something usable")
-                }
-            }
-        }
+			for _, commit := range event.Payload.Shas {
+				switch commitData := commit.(type) {
+				case []interface{}:
+					fmt.Println("Commit Message:", commitData[2])
+                    loc, err := event.GetActorLocation()
+                    if err != nil {
+                        log.Fatal(err)
+                    }
+                    fmt.Println(event.Actor, loc)
+				default:
+					log.Fatal("Could not convert Shas to something usable")
+				}
+			}
+		}
 
 	}
 }
